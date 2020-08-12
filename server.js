b66b15e5024ca20633e90238f9d3ce708b4954c7
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2020-08-12 18:52:49 +0300

    final changes

--- server.js
+++ server.js
@@ -9,74 +9,74 @@ app.get('/', (req, res) => {
 })
 
 // Color contrast paths
-app.get('/color', (req, res) => {
-    res.sendFile(path.join(__dirname + '/color-contrast/good-color-contrast.html'));
-    app.use(express.static(__dirname + '/color-contrast'))
+app.use('/color', (req, res) => {
+    app.use(express.static(__dirname + '/color-contrast'));
+    res.sendFile(path.join(__dirname + '/color-contrast/good-color-contrast.html'));  
 });
 
 app.get('/color-bad', (req, res) => {
+    app.use(express.static(__dirname + '/color-contrast'));
     res.sendFile(path.join(__dirname + '/color-contrast/bad-color-contrast.html'));
-    app.use(express.static(__dirname + '/color-contrast'))
 });
 
 // Color information
-app.get('/color-info', (req, res) => {
+app.get('/info', (req, res) => {
+    app.use(express.static(__dirname + '/communicate-info'));
     res.sendFile(path.join(__dirname + '/communicate-info/good-communicate-info.html'));
-    app.use(express.static(__dirname + '/communicate-info'))
 });
 
-app.get('/color-info-bad', (req, res) => {
+app.get('/info-bad', (req, res) => {
+    app.use(express.static(__dirname + '/communicate-info'));
     res.sendFile(path.join(__dirname + '/communicate-info/bad-communicate-info.html'));
-    app.use(express.static(__dirname + '/communicate-info'))
 });
 
 // Form labeling
 app.get('/form', (req, res) => {
+    app.use(express.static(__dirname + '/form-labeling'));
     res.sendFile(path.join(__dirname + '/form-labeling/good-form-labeling.html'));
-    app.use(express.static(__dirname + '/form-labeling'))
 });
 
 app.get('/form-bad', (req, res) => {
+    app.use(express.static(__dirname + '/form-labeling'));
     res.sendFile(path.join(__dirname + '/form-labeling/bad-form-labeling.html'));
-    app.use(express.static(__dirname + '/form-labeling'))
 });
 
 // Page structure
 app.get('/pagestructure', (req, res) => {
+    app.use(express.static(__dirname + '/page-structure'));
     res.sendFile(path.join(__dirname + '/page-structure/good-page-structure.html'));
-    app.use(express.static(__dirname + '/page-structure'))
 });
 
 app.get('/pagestructure-bad', (req, res) => {
+    app.use(express.static(__dirname + '/page-structure'));
     res.sendFile(path.join(__dirname + '/page-structure/bad-page-structure.html'));
-    app.use(express.static(__dirname + '/page-structure'))
 });
 
 // Images
 app.get('/images', (req, res) => {
+    app.use(express.static(__dirname + '/images'));
     res.sendFile(path.join(__dirname + '/images/images-good.html'));
-    app.use(express.static(__dirname + '/images'))
 });
 
 app.get('/images-bad', (req, res) => {
+    app.use(express.static(__dirname + '/images'));
     res.sendFile(path.join(__dirname + '/images/images-bad.html'));
-    app.use(express.static(__dirname + '/images'))
 });
 
 // Semantic HTML
 app.get('/semantic', (req, res) => {
+    app.use(express.static(__dirname + '/semantic-html'));
     res.sendFile(path.join(__dirname + '/semantic-html/good-html-semantic.html'));
-    app.use(express.static(__dirname + '/semantic-html'))
 });
 
 app.get('/semantic-aria', (req, res) => {
+    app.use(express.static(__dirname + '/semantic-html'));
     res.sendFile(path.join(__dirname + '/semantic-html/aria-html-semantic.html'));
-    app.use(express.static(__dirname + '/semantic-html'))
 });
 
 app.get('/semantic-bad', (req, res) => {
+    app.use(express.static(__dirname + '/semantic-html'));
     res.sendFile(path.join(__dirname + '/semantic-html/bad-html-semantic.html'));
-    app.use(express.static(__dirname + '/semantic-html'))
 });
 
 app.listen(port, (err) => {
